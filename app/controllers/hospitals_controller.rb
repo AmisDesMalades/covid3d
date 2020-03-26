fd5db8053d3fcd19f69847d551a583b89958e1be
sneakyweasel <philippe.cochin@gmail.com>
2020-03-26 01:51:05 +0100

    Added geolocation and close hospitals

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -5,7 +5,7 @@ class HospitalsController < ApplicationController
   # GET /hospitals.json
   def index
     @hospitals = Hospital.all
-    @hash = Gmaps4rails.build_markers(@hospitals) do |hospital, marker|
+    @markers  = Gmaps4rails.build_markers(@hospitals) do |hospital, marker|
       marker.lat hospital.latitude
       marker.lng hospital.longitude
     end
