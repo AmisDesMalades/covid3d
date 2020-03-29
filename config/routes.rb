e5359114fdd2e85c72ac27979975d774ccb318cb
sneakyweasel <philippe.cochin@gmail.com>
2020-03-29 03:14:38 +0200

    Cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,21 +9,10 @@ Rails.application.routes.draw do
   get '/groupes', to: 'home#groupes'
   get '/contact', to: 'home#contact'
   get '/soignant', to: 'home#soignant'
-
+  get '/urgent', to: 'home#urgent'
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-  get '/medics', to: 'home#medics'
-  get '/makers', to: 'home#makers'
-  get '/urgent', to: 'home#urgent'
   get '/autres_demandes', to: 'home#autres_demandes'
-  
-  get '/ingenieur_aide', to: 'home#ingenieur_aide'
-  get '/ingenieur_projets', to: 'home#ingenieur_projets'
-  
-  get '/soignant_projets', to: 'home#soignant_projets'
-  
-  get '/fabricant_aide', to: 'home#fabricant_aide'
-  get '/fabricant_projets', to: 'home#fabricant_projets'
 
   get '/projet-intubation', to: 'home#projet_intubation'
   get '/projet-visiere', to: 'home#projet_visiere'
