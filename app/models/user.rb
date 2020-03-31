6093b3d238ceaa05f28775aefc35502bcce6488e
sneakyweasel <philippe.cochin@gmail.com>
2020-03-31 02:06:47 +0200

    Restrictions admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+  devise :masqueradable, 
+    :database_authenticatable, 
+    # :registerable, 
+    :recoverable, 
+    :rememberable, 
+    :validatable, 
+    # :omniauthable
 
   has_person_name
 
