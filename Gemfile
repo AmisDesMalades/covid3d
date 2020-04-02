da96d364874935138272cdb60ca362a528d18b40
sneakyweasel <philippe.cochin@gmail.com>
2020-04-03 01:12:56 +0200

    Added pundit

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'geocoder'
 gem 'gmaps4rails'
 gem 'meta-tags'
-gem 'cancancan'
+gem 'pundit'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index]
+  
   # GET /projects
   # GET /projects.json
   def index
