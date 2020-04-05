122609f32abe401636951d9f913c9705f44f2895
Julien Grillot <julien.grillot@gmail.com>
2020-04-05 13:09:36 +0200

    Project policy

---
+++ app/policies/project_policy.rb
@@ -0,0 +1,27 @@
+class ProjectPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    false
+  end
+end
