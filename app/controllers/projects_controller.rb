cf88a8bf6622eb4ca76bad4d952e455d909ccba0
sneakyweasel <philippe.cochin@gmail.com>
2020-04-06 13:18:45 +0200

    ACL show project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /projects
   # GET /projects.json
