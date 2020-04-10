5ab9dc0720cc17a2f504bdcc4992de5d02a90403
Victor Goya <goya.victor@gmail.com>
2020-04-10 09:35:54 +0200

    nothing fancy, try to make things simpler

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def project_status_in_words(project)
+    {
+      "1" => "en cours de prototypage",
+      "2" => "en cours de validation",
+      "3" => "en cours de diffusion"
+    }[project.status]
+  end
 end
